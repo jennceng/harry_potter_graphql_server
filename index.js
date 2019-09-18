a6e3b6bbaa39388479ae488a0bf8d2df3b79a741
Jenn Eng <jenneng6@gmail.com>
2019-09-18 16:32:03 -0500

    starting point

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const { ApolloServer, gql } = require("apollo-server");
-
-const typeDefs = gql`
-  type Query {
-    hello: String
+let students = [
+  {
+    id: "988d60bb-6769-4b36-a26a-cea4da9f734c",
+    name: "Harry Potter",
+    guardian: "Aunt Petunia",
+    house: "GRYFFINDOR"
+  },
+  {
+    id: "5071094f-7a3f-4793-9e08-2ed17387f7b5",
+    name: "Ron Weasley",
+    guardian: "Molly Weasley",
+    house: "GRYFFINDOR"
   }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "world"
-  }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+];
