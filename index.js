5edfc4275d8dbd1298d090c17db66bf0f7daf15f
Jenn Eng <jenneng6@gmail.com>
2019-09-18 14:59:25 -0500

    starting point

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
